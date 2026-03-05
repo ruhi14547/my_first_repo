363f2257d20c669fcf7894e567418608565007ab
ruhi14547 <rutikas.mca25@siescoms.sies.edu.in>
2026-03-05 14:15:02 +0530

    Update jen1.java

--- jen1.java
+++ jen1.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class jen1{
     public static void main(String[]args){
         System.out.println("Hello Jenkins");
-        System.out.println("Hello rutika14");
+        System.out.println("Hello Nishi");
     }
 }
